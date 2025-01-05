22582964fed69f098c5d65c71623f8e4a3615757
DoTheBestMayB <send1684@gmail.com>
2025-01-05 20:34:01 +0900

    Oval GradientBackground 추가

--- app/src/main/java/com/dothebestmayb/composestudy/gradient_background/GradientBackgroundDemo.kt
+++ app/src/main/java/com/dothebestmayb/composestudy/gradient_background/GradientBackgroundDemo.kt
@@ -74,6 +74,68 @@ fun GradientBackgroundDemo(
     }
 }
 
+@Composable
+fun GradientBackgroundOvalDemo(
+    modifier: Modifier = Modifier,
+    endPercent: Float = 0.05f,
+    startColor: Color = MaterialTheme.colorScheme.secondary,
+    content: @Composable BoxScope.() -> Unit
+) {
+    Box(modifier) {
+        Box(
+            modifier = Modifier
+                .matchParentSize()
+                .drawWithCache {
+                    onDrawBehind {
+                        val endColor = Color.Transparent
+
+                        // 뷰포트의 최대 치수보다 크게 설정 (상단까지 부드럽게 퍼지도록)
+                        val radius = maxOf(size.width, size.height) * 0.4f
+
+                        // scale로 타원화(가로/세로 비율 조정)
+                        val scaleX = 1.55f // 가로는 많이 차지하도록 설정
+                        val scaleY = 0.7f
+
+                        // 그라디언트 중심: Box 하단 중앙
+                        val center = Offset(
+                            x = size.width / 2f,
+                            y = size.height * endPercent + radius * scaleY
+                        )
+
+                        // '원형' 그라디언트 브러시 (실제 사용 시엔 translate/scale로 타원화)
+                        val radialBrush = Brush.radialGradient(
+                            colors = listOf(startColor, endColor),
+                            center = Offset.Zero, // 실제 draw 때 옮길 예정
+                            radius = radius
+                        )
+
+                        // 그리기 시작
+                        drawContext.canvas.save()
+
+                        // 하단 가운데로 이동
+                        drawContext.canvas.translate(center.x, center.y)
+
+
+                        drawContext.canvas.scale(scaleX, scaleY)
+
+                        // 대형 사각형(원 중심 기준) 영역에 그라디언트 그리기
+                        drawRect(
+                            brush = radialBrush,
+                            topLeft = Offset(-radius, -radius),
+                            size = Size(2 * radius, 2 * radius)
+                        )
+
+                        drawContext.canvas.restore()
+                    }
+                }
+        )
+
+        // 최종적으로 이 안에 들어갈 실제 content
+        Box {
+            content()
+        }
+    }
+}
 
 @Preview(
     showBackground = true,
@@ -81,7 +143,7 @@ fun GradientBackgroundDemo(
 @Composable
 private fun GradientBackgroundDemoPreview() {
     ComposeStudyTheme {
-        GradientBackgroundDemo(
+        GradientBackgroundOvalDemo(
             startColor = Color(0x8026A69A)
         ) {
             Text(
