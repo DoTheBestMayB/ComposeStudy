ef3c6333d0f37c1837c3afa4942f88424b7d292e
DoTheBestMayB <send1684@gmail.com>
2025-09-27 21:51:36 +0900

    build: 버전 올리기

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.dothebestmayb.composestudy"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.dothebestmayb.composestudy"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
